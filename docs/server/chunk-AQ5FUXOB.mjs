9d7906709db01b100ec4e53b1a30a58075d0e5f2
kabeer1610 <kabeerahamed1610@gmail.com>
2024-01-25 00:11:22 +0530

    publish to github

---
+++ docs/server/chunk-AQ5FUXOB.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as n,c as s,d as p,n as m,r as a}from"./chunk-QBTRPB4U.mjs";var c=(()=>{let t=class t{constructor(){this.title="app-digital"}};t.\u0275fac=function(o){return new(o||t)},t.\u0275cmp=n({type:t,selectors:[["app-root"]],decls:1,vars:0,template:function(o,l){o&1&&p(0,"router-outlet")},dependencies:[a]});let e=t;return e})();var d=(()=>{let t=class t{};t.\u0275fac=function(o){return new(o||t)},t.\u0275mod=s({type:t,bootstrap:[c]}),t.\u0275inj=r({imports:[m]});let e=t;return e})();export{d as a};
