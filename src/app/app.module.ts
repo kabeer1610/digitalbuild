88d93407362a04f10f58e0e3c5b3869187b8e7a3
mohideen <sjmohideen@gmail.com>
2024-01-29 15:19:24 +0530

    added Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    HomeLayoutComponent,
+    LoginLayoutComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [
     provideClientHydration()
   ],
+  exports:[RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
